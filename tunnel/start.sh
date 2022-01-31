9da413ddd4994c4e4d5541dc3b30371dc3cc2a56
JTangT <ttj2350549626@gmail.com>
2022-02-01 07:53:48 +0800

    for test

--- tunnel/start.sh
+++ tunnel/start.sh
@@ -2,6 +2,6 @@ chmod +x gost
 chmod +x SecureTunnel
 killall -q gost
 killall -q SecureTunnel
-./gost -L ss://${TYPE}:${PASSWORD}@:8081 > /dev/null 2>&1 &
+./gost -L ss://${TYPE}:${PASSWORD}@:8081 &#> /dev/null 2>&1 &
 sed -i s/PORT/${PORT}/g ./config.json
 exec ./SecureTunnel -config config.json
